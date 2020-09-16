5ae7574c63920f46ab0a88f0ebe5a7e55e8da6e8
Kyle Morgan <kmorgan8588@gmail.com>
2020-09-16 12:43:44 -0700

    initial commit after create-react-app, init Form component

---
+++ src/components/ContactForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const ContactForm = (props) => {
+    return (
+        <div>
+            <h1>Contact Us</h1>
+        </div>
+    )
+};
+
+
+export default ContactForm;
